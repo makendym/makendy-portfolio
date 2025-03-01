138c14bb33c847a6a4d6ef135c8f1dc8764e9960
mmidouin <makendymidouin99@gmail.com>
2025-03-01 02:24:01 -0500

    Update site title to "Makendy Midouin"

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ import "./styles/globals.css";
 import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = {
-  title: "I am Makendy",
+  title: "Makendy Midouin",
   description: "Created by Makendy Midouin",
 };
 
