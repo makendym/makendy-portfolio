ddb4b30736a2a14beafea124ab66dee0b1ea510a
mmidouin <makendymidouin99@gmail.com>
2025-03-12 03:50:03 -0400

    Fix import path for SpeedInsights component in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import "./styles/globals.css";
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 export const metadata = {
   title: "Makendy Midouin",
   description: "Created by Makendy Midouin",
